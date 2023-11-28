fde6b17297dee2d1e64d43cf5e658714ad31103c
somberlyx <lybasiddiqui02@gmail.com>
2023-11-28 21:01:05 +0400

    Create section to display the results of calculations

--- calulator.js
+++ calulator.js
@@ -1,23 +1,46 @@
 const numbers = document.querySelectorAll(".numbers");
 const displaySelection = document.querySelector(".displaySelections")
+const displayResults = document.querySelector(".displayResults")
 let userInput = [];
 let userInput2 = [];
-let firstNum;
-let secondNum;
+let firstNum = 0;
+let secondNum = 0;
 let step = 1;
 let operation;
+let result = 0
 
 numbers.forEach(numbers => {
   numbers.addEventListener("click", function(){
     if (numbers.value === 'AC'){
       displaySelection.value = '';
+      firstNum = 0
+      secondNum = 0
+      result = 0
       userInput = [];
+      userInput2 = [];
+      operation = null
+      step = 1
+      displayResults.innerHTML = '';
     } else if (numbers.value === 'DE'){
       displaySelection.value = displaySelection.value.toString().slice(0,-1);
       userInput.pop();
     } else if (numbers.value === '+' || numbers.value === '*' || numbers.value === '/' || numbers.value === '-'){
       step = 2;
       operation = numbers.value;
+      console.log('first number', firstNum, 'second number', secondNum)
+      if (operation === '+') {
+        result = firstNum + secondNum;
+        displayResults.innerHTML = result;
+      } else if (operation === '-') {
+        result = firstNum - secondNum;
+        displaySelection.value = result;
+      } else if (operation === '*') {
+        result = firstNum * secondNum;
+        displaySelection.value = result;
+      } else if (operation === '/') {
+        result = firstNum / secondNum;
+        displaySelection.value = result;
+      }
     } else {
       if (step === 1){
         step = 1;
@@ -29,10 +52,8 @@ numbers.forEach(numbers => {
         secondNum = Number(userInput2.join(''))
         displaySelection.value = secondNum;
       }
-      
     }
     console.log(userInput);
-    evaluate();
   })
 })
 
