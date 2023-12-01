fbb8d3115546e9052da137676f4a6641a20c0277
somberlyx <lybasiddiqui02@gmail.com>
2023-12-01 17:32:01 +0400

    Calculate the answer for the first 2 numbers

--- calulator.js
+++ calulator.js
@@ -9,7 +9,7 @@ let step = 1;
 let operation;
 let result = 0
 
-numbers.forEach(numbers => {
+/* numbers.forEach(numbers => {
   numbers.addEventListener("click", function(){
     if (numbers.value === 'AC'){
       displaySelection.value = '';
@@ -52,6 +52,82 @@ numbers.forEach(numbers => {
         secondNum = Number(userInput2.join(''))
         displaySelection.value = secondNum;
       }
+    }
+    console.log(userInput);
+  })
+}) */
+
+let numEntered = '';
+
+numbers.forEach(numbers => {
+  numbers.addEventListener("click", function(){
+    if (numbers.value === 'AC'){
+      displaySelection.value = '';
+      firstNum = 0
+      secondNum = 0
+      result = 0
+      userInput = [];
+      userInput2 = [];
+      operation = null
+      step = 1
+      displayResults.innerHTML = '';
+    } else if (numbers.value === 'DE'){
+      displaySelection.value = displaySelection.value.toString().slice(0,-1);
+      userInput.pop();
+    } else if (numbers.value === '+' || numbers.value === '*' || numbers.value === '/' || numbers.value === '-'){
+      userInput.push(numEntered);
+      operation = numbers.value;
+      userInput2.push(operation)
+      numEntered = '';
+      /* if (operation === '+') {
+        result = firstNum + secondNum;
+        displayResults.innerHTML = result;
+      } else if (operation === '-') {
+        result = firstNum - secondNum;
+        displaySelection.value = result;
+      } else if (operation === '*') {
+        result = firstNum * secondNum;
+        displaySelection.value = result;
+      } else if (operation === '/') {
+        result = firstNum / secondNum;
+        displaySelection.value = result;
+      } */
+    } else if(numbers.value === '=') {
+      userInput.push(numEntered);
+      numEntered = '';
+      let calculation = 0;
+      userInput = userInput.map(Number);
+      for (i=0; i < userInput2.length; i++){
+        if (userInput2[i] === '+') {
+          result = userInput[0] + userInput[1]
+          calculation += result;
+          displayResults.innerHTML = calculation;
+        } else if (userInput2[i] === '-') {
+          result = userInput[0] - userInput[1];
+          calculation -= result;
+          displaySelection.value = calculation;
+        } else if (userInput2[i] === '*') {
+          result = userInput[0] * userInput[1];
+          calculation *= result;
+          displaySelection.value = calculation;
+        } else if (userInput2[i] === '/') {
+          result = userInput[0] / userInput[1];
+          calculation /= result
+          displaySelection.value = calculation;
+      }
+    }
+   } else {
+      numEntered += numbers.value;
+      /* if (step === 1){
+        step = 1;
+        userInput.push(numbers.value);
+        firstNum = Number(userInput.join(''))
+        displaySelection.value = firstNum;
+      } else if (step === 2){
+        userInput2.push(numbers.value);
+        secondNum = Number(userInput2.join(''))
+        displaySelection.value = secondNum;
+      } */
     }
     console.log(userInput);
   })
