961d7f6961e71592c84568d6eef162a6035ec339
somberlyx <lybasiddiqui02@gmail.com>
2023-12-01 21:05:13 +0400

    change userInput[] to numberArray[]
    formatted and added a few comments

--- calulator.js
+++ calulator.js
@@ -1,26 +1,26 @@
 const numbers = document.querySelectorAll(".numbers");
 const displaySelection = document.querySelector(".displaySelections")
 const displayResults = document.querySelector(".displayResults")
-let userInput = [];
+let numberArray = [];
 let operationArray = [];
 let operation;
 let result = 0;
 let numEntered = '';
-let j = 0;
+let j = 0; //Determines the position of where to start calculations in numberArray[]
 
 numbers.forEach(numbers => {
   numbers.addEventListener("click", function(){
     if (numbers.value === 'AC'){
       reset();
-    } else if (numbers.value === 'DE'){
+    } else if (numbers.value === 'DE'){ //removes the last number inputted from user.
       displaySelection.value = displaySelection.value.toString().slice(0,-1);
       numEntered = numEntered.toString().slice(0,-1);
     } else if (numbers.value === '+' || numbers.value === '*' || numbers.value === '/' || numbers.value === '-'){
       if (numEntered === ''){
         operation = numbers.value;
-        operationArray.push(operation)
+        operationArray.push(operation) //stores the operations that need to be executed
       } else {
-        userInput.push(numEntered);
+        numberArray.push(numEntered);
         operation = numbers.value;
         operationArray.push(operation)
         numEntered = '';
@@ -31,14 +31,14 @@ numbers.forEach(numbers => {
       numEntered += numbers.value;
       displaySelection.value = numEntered;
     }
-    console.log(userInput);
+    console.log(numberArray);
   })
 })
 
 function reset(){
   displaySelection.value = '';
   result = 0
-  userInput = [];
+  numberArray = [];
   operationArray = [];
   operation = null;
   numEntered = '';
@@ -47,36 +47,32 @@ function reset(){
 }
 
 function calculate(){
-  userInput.push(numEntered);
-      numEntered = '';
-      userInput = userInput.map(Number);
-      for(i = j; i < userInput.length; i++){
-        if (operationArray[i] === '+') {
-          result = userInput[i] + userInput[i+1]
-          userInput[i+1] = result;
-          result = userInput[i+1];
-          //calculation += result;
-          displayResults.innerHTML = result;
-        } else if (operationArray[i] === '-') {
-          result = userInput[i] - userInput[i+1];
-          userInput[i+1] = result;
-          result = userInput[i+1];
-          //calculation -= result;
-          displayResults.innerHTML = result;
-        } else if (operationArray[i] === '*') {
-          result = userInput[i] * userInput[i+1];
-          userInput[i+1] = result;
-          result = userInput[i+1];
-          //calculation *= result;
-          displayResults.innerHTML = result;
-        } else if (operationArray[i] === '/') {
-          result = userInput[i] / userInput[i+1];
-          userInput[i+1] = result;
-          result = userInput[i+1];
-          //calculation /= result
-          displayResults.innerHTML = result;
-        }
-        j++;
-      }
-      return j--;
+  numberArray.push(numEntered);
+  numEntered = '';
+  numberArray = numberArray.map(Number);
+  for(i = j; i < numberArray.length; i++){
+    if (operationArray[i] === '+') {
+      result = numberArray[i] + numberArray[i+1]
+      numberArray[i+1] = result;
+      result = numberArray[i+1];
+      displayResults.innerHTML = result;
+    } else if (operationArray[i] === '-') {
+      result = numberArray[i] - numberArray[i+1];
+      numberArray[i+1] = result;
+      result = numberArray[i+1];
+      displayResults.innerHTML = result;
+    } else if (operationArray[i] === '*') {
+      result = numberArray[i] * numberArray[i+1];
+      numberArray[i+1] = result;
+      result = numberArray[i+1];
+      displayResults.innerHTML = result;
+    } else if (operationArray[i] === '/') {
+      result = numberArray[i] / numberArray[i+1];
+      numberArray[i+1] = result;
+      result = numberArray[i+1];
+      displayResults.innerHTML = result;
+    }
+    j++;
+  }
+  return j--;
 }
